c58fd0d9a1602d66a54134b390f26fd6631574f1
majmalgot <pk.msn@msn.com>
2024-08-28 10:28:10 +0200

    Add script.py into Dev branch

---
+++ script.py
@@ -0,0 +1,19 @@
+
+print('Hello World!')
+
+name=input('Your name:')
+print(name)
+
+print('what is your name')
+name=input()
+print(name)
+
+x = input('Enter a number: ')
+print(type(x))
+
+x = int(input('Enter a number: '))
+print(type(x))
+
+first_number = int(input('Type the first number: ')) ;\
+second_number = int(input('Type the second number: ')) ;\
+print("The sum is: ", first_number + second_number)
